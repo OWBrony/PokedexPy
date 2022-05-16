4442b24b66b785d68fa40b5c8402de2847efaaf9
OWBrony <97199545+OWBrony@users.noreply.github.com>
2022-05-16 12:39:51 -0600

    Create object.py
    I made the object class that will be a blueprint for objects.

---
+++ items/object.py
@@ -0,0 +1,16 @@
+class Object:
+    def __init__(self):
+        """This is to create a new object"""
+        pass
+
+    def write_text():
+        """This is for writing the text on the screen"""
+        pass
+
+    def show_image():
+        """This is to show an image"""
+        pass
+
+    def set_position():
+        """This is to set the position of the object"""
+        pass
