5addd23935636d24770e2d64ee24054910365fc3
OWBrony <97199545+OWBrony@users.noreply.github.com>
2022-06-14 14:23:21 -0600

    Sprite Appears!
    The sprite of the pokemon now shows up. The default image does not disappear though.

--- items/image.py
+++ items/image.py
@@ -2,9 +2,13 @@ import pygame
 from items.artifact import Artifact
 from Constants import *
 
+import requests
+import json
+import io
 class Image(Artifact):
     def __init__(self, x=0, y=0):
         self.image = ""
+        self.image_from_URL = None
         self.position = ()
         self._x = x
         self._y = y
@@ -16,6 +20,12 @@ class Image(Artifact):
     def show_image(self):
         if self.image == IMAGE_DEFAULT:
             hold = pygame.image.load(self.image)
+            picture = pygame.transform.scale(hold, (MAX_WIDTH/2,MAX_HEIGHT/2))
+            self.display_surface.blit(picture, (0,0))
+        else:
+            hold_request = requests.get(self.image)
+            self.image_from_URL = io.BytesIO(hold_request.content)
+            hold = pygame.image.load(self.image_from_URL)
             picture = pygame.transform.scale(hold, (MAX_WIDTH/2,MAX_HEIGHT/2))
             self.display_surface.blit(picture, (0,0))
 
